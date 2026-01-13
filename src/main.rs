065ec19b3e838d5cb0eaf92c303280a7e1977b51
Alper Timucin Akyol <akyolalper101@gmail.com>
2026-01-13 16:11:10 +0300

    brand new roguelike project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use rltk::{Rltk, GameState};
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk){
+        ctx.cls();
+        ctx.print(1, 1, "HELLOWORLD");
+    }
+}
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50().with_title("Rogulike Build").build()?;
+    let gs = State{};
+    rltk::main_loop(context, gs)
+} 

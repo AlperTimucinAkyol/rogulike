6911b93f6f230eea4fc502d7c35bc6da6bb857ce
Alper Timucin Akyol <akyolalper101@gmail.com>
2026-01-18 19:17:59 +0300

    Player can move using keys

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use rltk::{GameState, RGB, Rltk}; // , VirtualKeyCode};
-use specs::prelude::*;
-// use std::cmp::{max,min};
+use rltk::{GameState, RGB, Rltk, VirtualKeyCode};
+use specs::prelude::*; //, rayon::iter::Positions};
 use specs_derive::Component;
+use std::cmp::{max, min};
 
 #[derive(Component)]
 struct Position {
@@ -21,11 +21,13 @@ struct State {
     ecs: World,
 }
 struct LeftWalker {}
+#[derive(Component)]
+struct Player {}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -58,6 +60,30 @@ impl State {
     }
 }
 
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x).clamp(0, 79)); //min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y).clamp(0, 49)); //min(49, max(0, pos.y + delta_y));
+        //why X 79 and Y 49 because we used ::simple80x50 BTermBuilder.
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -67,6 +93,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -75,6 +102,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..10 {

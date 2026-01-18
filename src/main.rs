5276911a86a05185a4a79d988d080da80e1b7ddc
Alper Timucin Akyol <akyolalper101@gmail.com>
2026-01-18 18:39:41 +0300

    left movers are now moving left

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,72 @@
-use rltk::{Rltk, GameState, RGB};// , VirtualKeyCode};
+use rltk::{GameState, RGB, Rltk}; // , VirtualKeyCode};
 use specs::prelude::*;
 // use std::cmp::{max,min};
 use specs_derive::Component;
 
 #[derive(Component)]
-struct Position{
-    x:i32,
-    y:i32,
+struct Position {
+    x: i32,
+    y: i32,
 }
 
 #[derive(Component)]
-struct Renderable{
+struct Renderable {
     glyph: rltk::FontCharType,
     fg: RGB,
     bg: RGB,
 }
+#[derive(Component)]
+struct LeftMover {}
 struct State {
     ecs: World,
 }
+struct LeftWalker {}
 
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk){
+    fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
-        for (pos, render) in (&positions,&renderables).join(){
-            ctx.set(pos.x,pos.y,render.fg,render.bg,render.glyph)
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph)
         }
     }
 }
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50().with_title("Rogulike Build").build()?;
-    let mut  gs = State{ecs: World::new()};
-    gs.ecs.register::<Position>();        
+    let context = RltkBuilder::simple80x50()
+        .with_title("Rogulike Build")
+        .build()?;
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-
+    gs.ecs.register::<LeftMover>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -49,15 +79,16 @@ fn main() -> rltk::BError {
 
     for i in 0..10 {
         gs.ecs
-        .create_entity()
-        .with(Position { x: i * 7, y: 20 })
-        .with(Renderable {
-            glyph: rltk::to_cp437('☺'),
-            fg: RGB::named(rltk::RED),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .build();
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(LeftMover {})
+            .build();
     }
 
     rltk::main_loop(context, gs)
-} 
+}

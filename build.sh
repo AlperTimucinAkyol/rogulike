fe6fe7632774e2c5882e4bb83e6977ab29814d93
Alper Timucin Akyol <akyolalper101@gmail.com>
2026-01-21 00:01:20 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #! /bin/bash
 
+set -e
+
+args1="$1"
+
 cargo build --release --target wasm32-unknown-unknown && \
 	wasm-bindgen target/wasm32-unknown-unknown/release/rogulike.wasm \
 	--out-dir wasm --no-modules --no-typescript
 
-if [ $pwd -eq  ~/rogulike/wasm ]; then
-	basic-http-server
-else
-	cd ~/rogulike/wasm
-	basic-http-server
+if [ $args1 = "--server" ]; then
+	if [ $pwd -eq  ~/rogulike/wasm ]; then
+		basic-http-server
+	else
+		cd ~/rogulike/wasm
+		basic-http-server
+	fi
 fi
 
 
